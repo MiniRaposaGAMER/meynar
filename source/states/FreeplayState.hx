52717d68d657caeaf43b81af8a59f4567deba3e0
Mini Raposa Gamer <115371844+MiniRaposaGAMER@users.noreply.github.com>
2024-11-08 21:46:12 -0300

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -107,7 +107,7 @@ class FreeplayState extends MusicBeatState
 		}
 		Mods.loadTopMod();
 
-		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg = new FlxSprite().loadGraphic(Paths.image('menuDesatFreeplay'));
 		bg.antialiasing = ClientPrefs.data.antialiasing;
 		add(bg);
 		bg.screenCenter();
